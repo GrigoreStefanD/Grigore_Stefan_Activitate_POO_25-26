116778f9810f7aa040621210626495d4b8d51a7b
GrigoreStefanD <stefangrigore07@gmail.com>
2025-10-24 10:03:49 +0300

    modificari

---
+++ GrigoreStefan_ActivitatePOO.cpp
@@ -0,0 +1,16 @@
+//
+//
+//#include <iostream>
+//
+//
+//using namespace std;
+//
+//
+//
+//int main () 
+//{
+//  
+//    cout << "Aasta e o verificare pt github" << endl;
+//   
+//    return 0;
+//}
